672ad41c1fc3fbb28b9d8be007d61e545a424a5f
buba <sas_bub@mail.ru>
2020-06-02 00:07:03 +0300

    add pug

--- src/express/index.js
+++ src/express/index.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const express = require(`express`);
+const path = require(`path`);
 
+const mainRoutes = require(`./routes/mainRoutes`);
 const registerRoutes = require(`./routes/register`);
 const loginRoutes = require(`./routes/login`);
 const myRoutes = require(`./routes/my`);
@@ -9,16 +11,24 @@ const offersRoutes = require(`./routes/offers`);
 const searchRoutes = require(`./routes/search`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
+
+console.log(path.resolve(__dirname, `templates`));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+// app.set(`views`, `./templates`);
+app.set(`view engine`, `pug`);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.use(`/register`, registerRoutes);
 app.use(`/login`, loginRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/offers`, offersRoutes);
 app.use(`/search`, searchRoutes);
-
-app.get(`/`, (req, res) => res.send(`/`));
+app.use(`/`, mainRoutes);
 
 app.listen(DEFAULT_PORT, () => {
   console.log(`Сервер запущен на порту ${DEFAULT_PORT}`);
